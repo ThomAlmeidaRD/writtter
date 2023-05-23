e3b21e23a6d40b1c97b38a6b969ae66f8df0a5e9
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-23 19:42:27 -0300

    add funcionalidade de visitar link do projeto

--- client/src/components/project.jsx
+++ client/src/components/project.jsx
@@ -1,9 +1,16 @@
-function Project(props){
-    return(
-        <div className="project">
-            <img src={props.print} alt="imagem de portfolio" />
+
+function Project(props) {
+
+    const handleLink = (project) => {
+        window.open(project.currentTarget.childNodes[3].href, 'blank')
+    }
+
+    return (
+        <div className="project" onClick={handleLink}>
+            <img src={props.print} alt="imagem de portfolio" draggable="false" />
             <h3>{props.name}</h3>
             <p>{props.desc}</p>
+            <a href={props.url} target="blank"></a>
             <ul>
                 <li>{props.langs}</li>
             </ul>
